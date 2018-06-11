90159974428884aa79adc8951258ec5469b0ff91
Deepanshu Lulla <dlulla@akamai.com>
2018-06-11 10:54:47 -0700

    heroku config

---
+++ src/main/java/com/flightreservation/flightreservation/configs/DatabaseConfig.java
@@ -0,0 +1,19 @@
+package com.flightreservation.flightreservation.configs;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
