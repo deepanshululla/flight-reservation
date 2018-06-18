30339b9fcc1ec6ce57f8115d21add18fd56fa9b8
Deepanshu Lulla <dlulla@akamai.com>
2018-06-17 23:03:40 -0700

    new jar

--- src/main/java/com/flightreservation/flightreservation/configs/DatabaseConfig.java
+++ src/main/java/com/flightreservation/flightreservation/configs/DatabaseConfig.java
@@ -8,12 +8,12 @@ import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
-@Configuration
-public class DatabaseConfig {
-    @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
-    }
-}
+//@Configuration
+//public class DatabaseConfig {
+//    @Bean
+//    @Primary
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource dataSource() {
+//        return DataSourceBuilder.create().build();
+//    }
+//}
