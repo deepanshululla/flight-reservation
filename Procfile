9075a75c5aee4ba351876543705e966bca81bf1f
Deepanshu Lulla <dlulla@akamai.com>
2018-06-17 21:28:53 -0700

    finally jar created

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/flightreservation-0.0.1-SNAPSHOT.jar
--- src/main/java/com/flightreservation/flightreservation/FlightreservationApplication.java
+++ src/main/java/com/flightreservation/flightreservation/FlightreservationApplication.java
@@ -1,7 +1,12 @@
 package com.flightreservation.flightreservation;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class FlightreservationApplication {
@@ -9,4 +14,18 @@ public class FlightreservationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FlightreservationApplication.class, args);
 	}
+//	@Bean
+//	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//		return args -> {
+//
+//			System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//			String[] beanNames = ctx.getBeanDefinitionNames();
+//			Arrays.sort(beanNames);
+//			for (String beanName : beanNames) {
+//				System.out.println(beanName);
+//			}
+//
+//		};
+//	}
 }
