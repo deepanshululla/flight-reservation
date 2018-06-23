686d99bf808e47543cb7dafed0df249a6d939f88
Deepanshu Lulla <dlulla@akamai.com>
2018-06-22 19:46:13 -0700

    changes

--- src/main/java/com/flightreservation/flightreservation/configs/DatabaseConfig.java
+++ src/main/java/com/flightreservation/flightreservation/configs/DatabaseConfig.java
@@ -1,12 +1,12 @@
-package com.flightreservation.flightreservation.configs;
+//package com.flightreservation.flightreservation.configs;
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-import javax.sql.DataSource;
+//import javax.sql.DataSource;
 
 //@Configuration
 //public class DatabaseConfig {
